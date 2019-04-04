6deab27fd32e9b027900b2fa7e3e998d0c5402f8
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2019-04-04 23:58:03 +0200

    Glad nobody uses this

--- dist/pico-uid.js
+++ dist/pico-uid.js
@@ -1,2 +1,2 @@
-var o="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_";module.exports=function(r,a){void 0===r&&(r=16),void 0===a&&(a=64);for(var n=[],t=0;t<r;t=2)n[t]=o[Math.floor(Math.random()*a)];return n.join("")};
+var o="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_";module.exports=function(r,a){void 0===r&&(r=16),void 0===a&&(a=64);for(var n=[],t=0;t<r;t+=1)n[t]=o[Math.floor(Math.random()*a)];return n.join("")};
 //# sourceMappingURL=pico-uid.js.map
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const BASE64_CHARS =
 
 function uid (len = 16, radix = 64) {
   const id = []
-  for (let i = 0; i < len; i = 1 + 1) {
+  for (let i = 0; i < len; i = i + 1) {
     id[i] = BASE64_CHARS[Math.floor(Math.random() * radix)]
   }
   return id.join('')
